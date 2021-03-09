b30870a6ce6162e5e6c8403fd81f2c6ae974758e
w-artisan <mailtome@i.ua>
2021-03-09 03:47:38 +0200

    feat: add validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, :email, presence: true
+  validates :email, uniqueness: true
+
+  validates :password, :password_confirmation, presence: true, on: :create
+  validates :password, confirmation: true
 end
