ba5c08685c5da3a9a366fac11038171cf94d14e6
w-artisan <mailtome@i.ua>
2021-03-09 04:25:53 +0200

    feat: add reference User - Comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,5 @@ class Comment < ApplicationRecord
   validates :content, presence: true
 
   belongs_to :blog
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_09_013557) do
+ActiveRecord::Schema.define(version: 2021_03_09_020608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,9 @@ ActiveRecord::Schema.define(version: 2021_03_09_013557) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "blog_id", null: false
+    t.bigint "user_id", default: 1, null: false
     t.index ["blog_id"], name: "index_comments_on_blog_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -44,4 +46,5 @@ ActiveRecord::Schema.define(version: 2021_03_09_013557) do
   end
 
   add_foreign_key "comments", "blogs"
+  add_foreign_key "comments", "users"
 end
