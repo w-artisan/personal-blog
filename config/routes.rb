d2e0751dd9f2e12a43817a41f59dd98fe6cb7b9e
w-artisan <mailtome@i.ua>
2021-03-09 02:06:35 +0200

    feat: add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'pages/home'
   get 'pages/about'
+  get 'about-me', to: 'pages#about'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
