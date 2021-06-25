66e528d4ac25b6231a83f6525c2749bf1eae985c
w-artisan <mailtome@i.ua>
2021-06-25 11:36:39 +0300

    add: before_action for #get_comments in BlogsController

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,6 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show :edit :update :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show, :get_comments]
 
   # GET /blogs or /blogs.json
   def index
