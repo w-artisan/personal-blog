be91d10de3193cd09362fc72e7eb8e1651366399
w-artisan <mailtome@i.ua>
2021-06-28 15:15:59 +0300

    remove: Routes for ActiveAdmin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users, path: "", path_names: {
     sign_in: "login",
