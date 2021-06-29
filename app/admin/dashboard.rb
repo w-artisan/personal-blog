b2e0b6a82c54a167738bf2e5ed9d9f47ee36abeb
w-artisan <mailtome@i.ua>
2021-06-29 14:26:10 +0300

    add: Update the Active Admin dashboard with the Users, Blogs and Comments

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,32 +1,39 @@
+# app/admin/dashboard.rb
 ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    # Here is an example of a simple dashboard with columns and panels.
+    columns do
+      panel "Info" do
+        para "Welcome to ActiveAdmin."
+      end
+      column do
+        panel "Recent Users" do
+          ul do
+            User.last(5).reverse.map do |user|
+              li link_to(user.email, admin_user_path(user))
+            end
+          end
+        end
+        panel "Recent Blogs" do
+          ul do
+            Blog.last(5).reverse.map do |blog|
+              li link_to(blog.title, admin_blog_path(blog))
+            end
+          end
+        end
+      end
+
+      column do
+        panel "Recent Comments" do
+          ul do
+            Comment.last(5).reverse.map do |comment|
+              li link_to(comment.content, admin_comment_path(comment))
+            end
+          end
+        end
       end
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+  end
 end
