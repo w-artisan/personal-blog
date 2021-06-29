0249628d316895e1bae726711b6cf7ae50b13001
w-artisan <mailtome@i.ua>
2021-06-29 16:46:32 +0300

    add: callbacks for Blog Model - discard methods

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,11 +1,19 @@
 class Blog < ApplicationRecord
   extend FriendlyId
-  include Discard::Model
+  friendly_id :title, use: :slugged
 
- friendly_id :title, use: :slugged
+  include Discard::Model
   # validates_presence_of :title, :body
   validates :title, :body, presence: true
 
   has_many :comments, dependent: :destroy
   belongs_to :user
+
+  after_discard do
+    comments.discard_all
+  end
+
+  after_undiscard do
+    comments.undiscard_all
+  end
 end
