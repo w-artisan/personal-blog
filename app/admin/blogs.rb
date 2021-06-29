c65a40c83423a5c062cb6710e82a4dc7e80594b6
w-artisan <mailtome@i.ua>
2021-06-29 10:58:02 +0300

    add: Add Blogs and Comments to Active Admin

---
+++ app/admin/blogs.rb
@@ -0,0 +1,19 @@
+ActiveAdmin.register Blog do
+  permit_params :title, :body, :user_id
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :title, :body, :user_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:title, :body, :user_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+
+end
